c137e25b5b5cfbee72547cb743fcc1aafed56311
Gary Hoppeler <hogary@student.ethz.ch>
2024-05-02 12:40:16 +0200

    ineffcient mapping, 2x const regions

--- src/delt_core/process/compute_counts.py
+++ src/delt_core/process/compute_counts.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 import shutil
 import subprocess
+import typing as tp
 
 import numpy as np
 
@@ -10,7 +11,6 @@ from utils import read_json, read_txt
 
 def compute_counts(
         components: dict,
-        const: dict,
         config: dict,
 ) -> None:
     """
@@ -25,24 +25,64 @@ def compute_counts(
     fastq_file = config['fastq_file']
     output_file = config['output_file']
 
-    structure = read_json(struct_file)
-
-    indices = map_sequences(components, consts, structure, fastq_file, output_file)
+    indices = map_sequences(components, fastq_file, output_file)
     print(indices)
 
     counts = {}
+
     return counts
+
+
+def run_cutadapt(
+        sequences: tp.List[str],
+        mode: str,
+        input_file: str,
+        info_file: str,
+        output_file: str = '/dev/null',
+        max_error_rate: float = 0.1,
+        min_overlap: float = 0.5,
+) -> None:
+    """
+    Mode:
+    -a: 3' adapter
+    -g: 5' adapter
+    """
+    min_overlap = round(len(sequences[0]) * min_overlap)
+    cmd = f'cutadapt -o {output_file} {input_file} --info-file={info_file} ' \
+          f'-e {max_error_rate} -O {min_overlap} --trimmed-only' # --quiet
+    for sequence in sequences:
+        cmd += f' -{mode} {sequence}'
+    subprocess.run(cmd.split(), stdout=subprocess.DEVNULL)
+
+
+def update_indices(
+        indices: np.ndarray,
+        info_file: str,
+        comp_idx: int,
+) -> None:
+    
+    reads = read_txt(info_file)
+    
+    for read in reads:
+        read = read.split('\t')
+        match = int(read[1]) >= 0
+        
+        if match:
+            read_idx = int(read[0].split('-')[-1])
+            seq_idx = int(read[7]) - 1
+            indices[comp_idx, read_idx] = seq_idx
+
+
+def update_output():
+    pass
 
 
 def map_sequences(
         components: dict,
-        consts: dict,
-        structure: str,
         fastq_file: str,
         output_file: str,
-        max_error_rate: float = .0,
-        epsilon_len: int = 0,
-        epsilon_dist: int = 0,
+        max_error_rate: float = 0.1,
+        min_overlap: int = 0.8,
 ) -> np.ndarray:
     
     parent = Path(output_file).parent
@@ -52,80 +92,63 @@ def map_sequences(
 
     num_reads = len(read_txt(fastq_file)) // 4
     indices = np.full((len(components), num_reads), -1)
-
-    # for const_idx, (const, seqs) in enumerate(consts.items()):
-
-    prev_comps = []
+    pre_comps = []
 
     for comp_idx, (component, seqs) in enumerate(components.items()):
+
+        # Map the rightmost region.
+        if comp_idx == len(components) - 1:
+            run_cutadapt(
+                sequences=seqs,
+                mode='g',
+                input_file=tmp,
+                info_file=info_file,
+                max_error_rate=max_error_rate,
+                min_overlap=min_overlap,
+            )
+            update_indices(indices, info_file, comp_idx)
+
+        # Map the leftmost constant region of the remaining sequence.
         if component[0] != 'C':
-            prev_comps += [component]
+            pre_comps += [component]
             continue
         
-        const = seqs[0]
-
-        # 3'
-        min_overlap = len(seqs[0]) - epsilon_len
-        cmd = f'cutadapt -o {output_file} {tmp} --info-file={info_file} ' \
-              f'-e {max_error_rate} -O {min_overlap} --trimmed-only -a {const}'
-        subprocess.run(cmd.split(), stdout=subprocess.DEVNULL)
+        run_cutadapt(
+            sequences=seqs,
+            mode='a',
+            input_file=tmp,
+            info_file=info_file,
+            output_file=output_file,
+            max_error_rate=max_error_rate,
+            min_overlap=min_overlap,
+        )
+        update_indices(indices, info_file, comp_idx)
         
-        for prev_comp in prev_comps:
-            seqs = components[prev_comp]
-            min_overlap = len(seqs[0]) - epsilon_len
-            cmd = f'cutadapt -o /dev/null {output_file} --info-file={info_file} ' \
-                f'-e {max_error_rate} -O {min_overlap} --trimmed-only'
-            for seq in seqs:
-                cmd += f' -g {seq}'
-            subprocess.run(cmd.split(), stdout=subprocess.DEVNULL)
+        # Map the sequence preceding the constant region.
+        for i, pre_comp in enumerate(pre_comps):
+            run_cutadapt(
+                sequences=components[pre_comp],
+                mode='g',
+                input_file=output_file,
+                info_file=info_file,
+                max_error_rate=max_error_rate,
+                min_overlap=min_overlap,
+            )
+            update_indices(indices, info_file, comp_idx - (len(pre_comps) - i))
+        pre_comps = []
 
-            reads = read_txt(info_file)
-
-            for read in reads:
-                read = read.split('\t')
-                read_idx = int(read[0].split('-')[-1])
-                match = int(read[1]) >= 0
-
-                if match:
-                    seq_idx = int(read[7]) - 1
-                    indices[comp_idx - 1, read_idx] = seq_idx
-        
-
-        # 5'
-        min_overlap = len(seqs[0]) - epsilon_len
-        cmd = f'cutadapt -o {output_file} {tmp} --info-file={info_file} ' \
-              f'-e {max_error_rate} -O {min_overlap} --trimmed-only -g {const}' # --quiet
-
-        # for seq in seqs:
-                # cmd += f' -g X{seq}' # non-internal 5’ adapter
-                # cmd += f' -g ^{seq}' # anchor 5’ adapter
-                # cmd += f' -g {seq}' # regular 5’ adapter
-
-        subprocess.run(cmd.split(), stdout=subprocess.DEVNULL)
-
-        reads = read_txt(info_file)
-
-        for read in reads:
-            read = read.split('\t')
-            read_idx = int(read[0].split('-')[-1])
-            match = int(read[1]) >= 0
-
-            if match:
-                seq_idx = int(read[7]) - 1
-                indices[comp_idx, read_idx] = seq_idx
-                continue
-
-                start = int(read[2])
-                end = int(read[3])
-                error_len = abs(end - start - len(seq))
-                error_dist = abs(start - structure[component])
-                
-                if error_len <= epsilon_len and error_dist <= epsilon_dist:
-                    indices[comp_idx, read_idx] = seq_idx
-                    # print(seq, read_idx, seq_idx, read)
-        
+        # Keep the sequence following the constant region.
+        run_cutadapt(
+            sequences=seqs,
+            mode='g',
+            input_file=tmp,
+            info_file=info_file,
+            output_file=output_file,
+            max_error_rate=max_error_rate,
+            min_overlap=min_overlap,
+        )
         shutil.copyfile(output_file, tmp)
-    
+
     os.remove(tmp)
     return indices
 
@@ -134,10 +157,9 @@ if __name__ == '__main__':
     
     path = Path('/Users/Gary/Downloads/zivi/git/delt-core/data/simulation')
     components = read_json(path / 'structure/components.json')
-    consts = read_json(path / 'structure/consts.json')
     config = read_json(path / 'config/config_cutadapt.json')
 
-    counts = compute_counts(components, consts, config)
+    counts = compute_counts(components, config)
 
     # print(sum(counts.values()))
 
