e618f996e659a9baff6b8db9bcde1f7edae8ffa2
adrianomartinelli <adrianom@student.ethz.ch>
2024-05-04 17:56:01 +0200

    initial darft to compute counts table with errors and exact matches

--- data/demultiplex/tested-commands.sh
+++ data/demultiplex/tested-commands.sh
@@ -56,3 +56,47 @@ cutadapt \
 -g file:p2.fasta \
 -o out7.fastq out6.fastq \
 --info-file=info.tsv
+
+# only match exact matches
+cutadapt -e 0 -O 10 \
+-g AAAAAAAAAA \
+-o out.short.fastq short_reads.fastq \
+--info-file=info.tsv
+
+# allow 1 SNPs
+cutadapt -e 0.1 -O 10 \
+-g AAAAAAAAAA \
+-o out.short.fastq short_reads.fastq \
+--info-file=info.tsv
+
+# allow 2 SNPs
+cutadapt -e 0.2 -O 10 \
+-g AAAAAAAAAA \
+-o out.short.fastq short_reads.fastq \
+--info-file=info.tsv
+
+# allow del only
+# however, this should allow for SNPs at the ends of the reads as well
+# but I observe that it does only for SNPs at the end not the start of the read
+cutadapt -e 0 -O 9 \
+-g AAAAAAAAAA \
+-o /dev/null short_reads.fastq \
+--info-file=info.tsv
+
+cutadapt -e 0 -O 9 \
+-g AAAAAAAAAA \
+-o /dev/null input.fastq \
+--info-file=info.tsv
+
+# demultiplex in different files
+cutadapt -e 0 -O 6 \
+-g file:p1.fasta \
+-a file:p2.fasta \
+-o out-{name}.fastq simlation.fastq \
+--info-file=info.tsv
+
+# sequential demultiplexing with exact matches
+cutadapt -e 0 \
+-g "file:p1.fasta;min_overlap=6" \
+-o out1.fastq simlation.fastq \
+--discard-untrimmed
