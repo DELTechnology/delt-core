2fb85956987bfd6692359d7ac9b968a866f044c1
Gary Hoppeler <hogary@student.ethz.ch>
2024-06-06 13:06:54 +0200

    test

--- data/demultiplex/examples/demultiplex-no-errors-allowed.sh
+++
@@ -1,6 +0,0 @@
-# sequential demultiplexing with exact matches
-cutadapt -e 0 \
--g "AAA;min_overlap=3" \
--o out.fastq input.fastq \
---rename '{id} {comment} {adapter_name}' \
---info-file=info.tsv
