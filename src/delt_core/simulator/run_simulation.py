6d7e861ee43a72a698f5214314fd7357f98f89c8
adrianomartinelli <adrianom@student.ethz.ch>
2024-06-09 19:03:37 +0200

    fix branch

--- src/delt_core/simulator/run_simulation.py
+++ src/delt_core/simulator/run_simulation.py
@@ -1,61 +1,61 @@
-# %%
-import json
 from pathlib import Path
+import typing as tp
 
 import click
 import numpy as np
 import pandas as pd
 
-# %%
-BASES = {'A', 'T', 'C', 'G'}
+import utils
+from delt_core.compute.compute_counts import perform_selection, create_table, save_counts
+
+
+BASES = ['A', 'T', 'C', 'G']
 rng = np.random.default_rng()
 
 
-def read_struct_file(path_to_struct_file):
-    # path_to_struct_file = Path('/Users/adrianomartinelli/projects/DECLDecoder/simulation/structureNF2.txt')
-    path_to_struct_file = Path(path_to_struct_file)
-    with open(path_to_struct_file, 'r') as f:
-        lines = f.readlines()
+def read_struct_file(
+        path: str,
+) -> tp.Dict:
+    
+    structure = utils.read_json(path)
+    elements = []
+    start = 0
 
-    file_name = lines[0].strip()
-    path_to_output = path_to_struct_file.parent / file_name
-    elements = list(map(lambda x: x.strip().split('\t'), lines[2:]))
-    elements = pd.DataFrame.from_records(elements, columns=['start', 'end', 'region_type', 'path_to_barcode_list'])
-    elements['start'] = elements['start'].astype(int) - 1
-    elements['end'] = elements['end'].astype(int) - 1
-    elements['path_to_barcode_list'] = [path_to_struct_file.parent / i for i in elements['path_to_barcode_list']]
+    for element, values in structure.items():
+        seqs = list(map(lambda x: [*x], values['Sequences']))
+        length = len(seqs[0])
+        end = start + length - 1
+        elements += [[start, end, element[0], seqs]]
+        start += length
 
-    barcodes = [open(i, 'r').readlines() for i in elements['path_to_barcode_list']]
-    barcodes = map(lambda x: list(map(lambda y: y.strip(), x)), barcodes)
-    barcodes = map(lambda x: filter(lambda y: len(y) > 0, x), barcodes)
-    barcodes = map(lambda x: list(map(lambda y: list(y), x)), barcodes)
-    barcodes = list(barcodes)
-    elements['barcodes'] = barcodes
-
-    elements = elements.sort_values('start')
-    return dict(file_name=file_name,
-                path_to_output=path_to_output,
-                elements=elements)
+    elements = pd.DataFrame.from_records(elements, columns=['start', 'end', 'region_type', 'barcodes'])
+    return (dict(elements=elements))
 
 
 def generate_reads(config):
-    number_of_reads = config['number_of_reads']
+    number_of_reads = config['num_reads']
     elements = config['elements']
     regions = elements[['start', 'end', 'barcodes']].to_records(index=False)
 
     read_length = elements.end.max() + 1
-    n = 0
     reads = []
-    while n < number_of_reads:
+    indices = np.full((len(regions), number_of_reads), -1)
+
+    for i in range(number_of_reads):
         read = np.array(['X'] * read_length)
-        for region in regions:
+
+        for j, region in enumerate(regions):
             start, end, barcodes = region
-            barcode = rng.choice(barcodes)
+            idx = rng.choice(range(0, len(barcodes[:10])))
+            # idx = rng.choice(range(0, len(barcodes)))
+            barcode = barcodes[idx]
+            indices[j, i] = idx
             assert end - start + 1 == len(barcode)
             read[start:(end + 1)] = barcode
+        
         reads.append(read)
-        n += 1
-    return reads
+
+    return reads, indices
 
 
 def create_all_barcode_regions_single_position_error(reads, elements, relative_position):
@@ -63,16 +63,17 @@ def create_all_barcode_regions_single_position_error(reads, elements, relative_p
     reads_stacked = np.stack(reads)
     regions = elements[elements.region_type.isin(['B'])]
     positions = regions.start + relative_position
-    reads_stacked[:, positions.values] = 'X'
+    base = rng.choice(BASES)
+    reads_stacked[:, positions.values] = base
     return [i for i in reads_stacked]
 
 
 def create_insertion_at_read_start_error(reads, elements, insertion_length, min_length=None, max_length=None, p=None):
     if insertion_length == 'random':
-        inserts = [np.array(['X'] * l) for l in rng.choice(range(min_length, max_length+1), size=len(reads), p=p)]
+        inserts = [np.array([rng.choice(BASES)] * l) for l in rng.choice(range(min_length, max_length+1), size=len(reads), p=p)]
         reads = [np.insert(read, 0, insert) for read, insert in zip(reads, inserts)]
     else:
-        insert = np.array(['X'] * insertion_length)
+        insert = np.array([rng.choice(BASES)] * insertion_length)
         reads = list(map(lambda x: np.insert(x, 0, insert), reads))
     return reads
 
@@ -84,15 +85,15 @@ def create_errors(config, reads):
         if error['type'] == 'all_barcode_regions_single_position':
             reads = create_all_barcode_regions_single_position_error(reads, elements, **error['kwargs'])
         elif error['type'] == 'insertion_at_read_start':
+            pass
             reads = create_insertion_at_read_start_error(reads, elements, **error['kwargs'])
         else:
             raise NotImplementedError()
-
     return reads
 
 
 def generate_fastq_file(config, reads):
-    path_to_output = config['path_to_output']
+    path_to_output = config['output_file']
     content = []
     for i, read in enumerate(reads):
         phred_scores = '~' * len(read)
@@ -105,20 +106,37 @@ def generate_fastq_file(config, reads):
         f.writelines(content)
 
 
-# %%
-@click.command()
-@click.argument('path_to_config', type=click.Path(exists=True))
-def main(path_to_config):
-    path_to_config = Path(path_to_config)
-    with open(path_to_config, 'r') as f:
-        config = json.load(f)
-    path_to_struct_file, number_of_reads = config['path_to_struct_file'], config['number_of_reads']
-    struct_dict = read_struct_file(path_to_struct_file)
+def run_simulation(
+        config: tp.Dict,
+) -> None:
+    
+    # Generate FASTQ file.
+    struct_file = config['struct_file']
+    struct_dict = read_struct_file(struct_file)
     config.update(struct_dict)
-    reads = generate_reads(config)
+    reads, indices = generate_reads(config)
     reads = create_errors(config, reads)
     generate_fastq_file(config, reads)
+
+    # Generate table for counts.
+    output_path = Path(config['output_file']).parent / 'counts_true'
+    output_path.mkdir(parents=True, exist_ok=True)
+    structure = utils.read_json(struct_file)
+    selections = perform_selection(structure, indices)
+    for selection in selections:
+        s1, s2 = selection[0]
+        counts = create_table(structure, selection[1])
+        save_counts(counts, output_path / f'counts_{s1}_{s2}.txt')
+
+
+@click.command()
+@click.argument('config_file', type=click.Path(exists=True))
+def main(config_file):
+    config = utils.read_json(config_file)
+    run_simulation(config)
+
 
 
 if __name__ == '__main__':
     main()
+
