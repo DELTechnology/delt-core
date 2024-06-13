773090b7e616270df2d64770d9d363605be183d3
Gary Hoppeler <hogary@student.ethz.ch>
2024-06-13 11:11:24 +0200

    add report and plot functions

--- src/delt_core/cli/main.py
+++ src/delt_core/cli/main.py
@@ -1,6 +1,7 @@
 import click
 
 from . import compute as c
+from . import quality_control as q
 
 
 @click.group()
@@ -9,8 +10,9 @@ def cli():
 
 
 cli.add_command(c.compute)
-cli.add_command(c.evaluate)
+cli.add_command(q.qc)
 
 
 if __name__ == '__main__':
     cli()
+
