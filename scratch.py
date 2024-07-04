08ac6b5e8af8020ec0fc09f721d3293c0243951e
adrianomartinelli <adrianom@student.ethz.ch>
2024-07-04 20:56:12 +0200

    dirty file to make quick tests

---
+++ scratch.py
@@ -0,0 +1,11 @@
+from delt_core.quality_control.compare_output import compare_counts_with_legacy
+from delt_core.demultiplex.preprocess import read_yaml
+from pathlib import Path
+
+from delt_core.cli.demultiplex.cmds import init
+init('', '', '', '', '', '')
+
+config = read_yaml(Path('/Users/adrianomartinelli/polybox/decl-data/data_andy/experiments/no-errors/config.yml'))
+config = read_yaml(Path('/Users/adrianomartinelli/polybox/decl-data/data_andy/experiments/ignore-C1/config.yml'))
+legacy_results_dir = Path('/Users/adrianomartinelli/polybox/decl-data/data_andy/evaluation_cpp')
+compare_counts_with_legacy(config, legacy_results_dir)
