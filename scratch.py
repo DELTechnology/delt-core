92833e5fddb3467de9444302330d4fb585c1d1fc
adrianomartinelli <adrianom@student.ethz.ch>
2024-07-04 23:14:46 +0200

    dirty file for quick debugs

--- scratch.py
+++ scratch.py
@@ -2,8 +2,12 @@ from delt_core.quality_control.compare_output import compare_counts_with_legacy
 from delt_core.demultiplex.preprocess import read_yaml
 from pathlib import Path
 
-from delt_core.cli.demultiplex.cmds import init
-init('', '', '', '', '', '')
+# from delt_core.cli.demultiplex.cmds import init
+#
+# from delt_core.demultiplex.preprocess import convert_struct_file
+# convert_struct_file('/Users/adrianomartinelli/polybox/decl-data/raw-files/structureNF2.txt')
+#
+# init('', '', '', '', '', '')
 
 config = read_yaml(Path('/Users/adrianomartinelli/polybox/decl-data/data_andy/experiments/no-errors/config.yml'))
 config = read_yaml(Path('/Users/adrianomartinelli/polybox/decl-data/data_andy/experiments/ignore-C1/config.yml'))
