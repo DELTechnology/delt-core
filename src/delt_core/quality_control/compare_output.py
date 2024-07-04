59799246b7f0c5574892ad6362c6fe6d2f8b9167
adrianomartinelli <adrianom@student.ethz.ch>
2024-07-04 20:55:57 +0200

    draft function to compare results with legacy program

--- src/delt_core/quality_control/compare_output.py
+++ src/delt_core/quality_control/compare_output.py
@@ -2,29 +2,39 @@ from pathlib import Path
 
 import pandas as pd
 
-o_dir1 = Path(
-    '/Users/adrianomartinelli/polybox - Adriano Martinelli (adriano.martinelli@pharma.ethz.ch)@polybox.ethz.ch/decl-data/raw-files/Evaluation_2301_704504_NF2-AG_yOST/')
-o_dir2 = Path(
-    '/Users/adrianomartinelli/Library/CloudStorage/OneDrive-ETHZurich/oneDrive-documents/data/delt/run-e0/eval/')
+from .. import demultiplex as d
 
-assert len(list(o_dir1.glob('selection*.txt'))) == len(list(o_dir2.glob('*_*.tsv')))
 
-for s1_path in o_dir1.glob('selection_*.txt'):
-    s1 = pd.read_csv(s1_path, sep='\t')
-
-    s1_name = s1_path.stem
-    s2_name = '_'.join(map(lambda x: str(int(x) - 1), filter(len, s1_name.split('_')[1:])))
-
-    s2_path = o_dir2 / (s2_name + '.tsv')
-    assert s2_path.exists()
-
-    s2 = pd.read_csv(s2_path, sep='\t')
-    s2 = s2.rename(columns=dict(zip(['count', 'barcode1', 'barcode2'], ['Count', 'Code1', 'Code2'])))
-    s2 = s2.assign(Code1=s2.Code1 + 1, Code2=s2.Code2 + 1)
+def counts_are_identical(results: Path, legacy_results):
+    legacy_results = legacy_results.rename(
+        columns=dict(zip(['count', 'barcode1', 'barcode2'], ['Count', 'Code1', 'Code2'])))
+    legacy_results = legacy_results.assign(Code1=legacy_results.Code1 + 1, Code2=legacy_results.Code2 + 1)
 
     cols = ['Count', 'Code1', 'Code2']
-    s1 = s1[cols].sort_values(cols)
-    s2 = s2[cols].sort_values(cols)
+    results = results[cols].sort_values(cols)
+    legacy_results = legacy_results[cols].sort_values(cols)
 
-    if not (s1 == s2).all().all():
-        print(f'Evaluation results differ for original {s1_path}.name')
+    return (results == legacy_results).all().all()
+
+
+def get_selection_primer_ids_from_legacy_identifier(selection_name: str):
+    return [int(i) - 1 for i in filter(len, selection_name.replace('selection_', '').split('_'))]
+
+
+def compare_counts_with_legacy(config: dict, legacy_results_dir: Path):
+    root = Path(config['Root']) / 'evaluations'
+    hash = d.preprocess.hash_dict(config['Structure'])
+
+    # assert len(list(legacy_results_dir.glob('selection*.txt'))) == len(list(root.glob(f'selection-*/{hash}.txt')))
+
+    for legacy_result_path in legacy_results_dir.glob('*.txt'):
+        selection_primer_ids = get_selection_primer_ids_from_legacy_identifier(legacy_result_path.stem)
+        selection_id = d.postprocess.get_selection_ids([selection_primer_ids], config=config)[0]
+
+        results_legacy = pd.read_csv(legacy_result_path, sep='\t')
+        result_path = root / f'selection-{selection_id}' / f'{hash}.txt'
+        assert result_path.exists()
+        results = pd.read_csv(result_path, sep='\t')
+
+        if not counts_are_identical(results, results_legacy):
+            print(f'Evaluation results differ for original {legacy_result_path.name}')
