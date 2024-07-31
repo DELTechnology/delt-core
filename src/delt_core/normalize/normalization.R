88dd92311e7d5835edee130fc46bee96a0d08e81
Gary Hoppeler <hogary@student.ethz.ch>
2024-07-31 18:52:20 +0200

    update R script

--- src/delt_core/normalize/normalization.R
+++ src/delt_core/normalize/normalization.R
@@ -1,11 +1,47 @@
 library('DESeq2')
+library('tidyverse')
 
-setwd('/Users/Gary/Downloads/zivi/data/normalization')
-file = 'data/counts.txt'
-cts = as.matrix(read.csv(file, sep='\t', header=TRUE, row.names='Code'))
+# FUNCTIONS
+read_selection = function(file) {
+  selection = read.table(file, header=TRUE, sep='\t')
+  num_cols = dim(selection)[2]
+  selection$Code = selection$Code1
+  for (col in 3:(num_cols)) {
+    selection$Code = paste(selection$Code, selection[, col], sep='-')
+  }
+  return(selection[, c('Count', 'Code')])
+}
+
+create_table = function(files) {
+  selections = data.frame(Code=character())
+  for (i in seq_along(files)) {
+    selection = read_selection(files[i])
+    suffixes = c(paste('_', files[i-1], sep=''), paste('_', files[i], sep=''))
+    selections = merge(selections, selection, by='Code', all.x=TRUE, all.y=TRUE, suffixes=suffixes)
+  }
+  selections[is.na(selections)] = 0
+  rownames(selections) = selections$Code
+  return(selections[, -1])
+}
+
+
+# MAIN
+path = '/Volumes/data_shared/data/shared/AM_Andy_DataAnalysisQuatsch/Data Campaign AG_21'
+setwd(path)
+
+files = c(
+  'selection_12_.txt', # protein
+  'selection_13_.txt', # protein
+  'selection_14_.txt', # protein
+  'selection_8_.txt',  # control
+  'selection_19_.txt', # control
+  'selection_20_.txt'  # control
+)
+
+cts = create_table(files)
 
 coldata = data.frame(
-  condition = c('protein', 'protein', 'protein', 'control', 'control', 'control'),
+  condition = c(rep('protein', 3), rep('control', 3)),
   row.names = colnames(cts)
 )
 coldata$condition = factor(coldata$condition)
@@ -15,15 +51,75 @@ dds = DESeqDataSetFromMatrix(cts, coldata, ~ condition)
 group_size = 3
 filter = rowSums(counts(dds) >= 10) >= group_size
 dds = dds[filter, ]
-
 dds$condition = relevel(dds$condition, ref = 'control')
 
 dds = DESeq(dds)
 
 res = results(dds, name='condition_protein_vs_control')
-resultsNames(dds)
+# plotCounts(dds, gene=which.min(res$padj), intgroup="condition")
 resLFC = lfcShrink(dds, coef='condition_protein_vs_control', type='apeglm')
 
-resOrdered = resLFC[order(resLFC$pvalue), ]
-sum(resOrdered$padj < 0.1, na.rm=TRUE)
-head(as.data.frame(resOrdered))
+resOrdered = resLFC[order(resLFC$padj), ]
+resOrdered$neglogp = -log10(resOrdered$padj)
+mask = resOrdered$padj < 0.05
+resSign = resOrdered[!is.na(mask) & mask, ]
+
+head(as.data.frame(resSign))
+# write.csv(resSign, file = "stats.csv", row.names = TRUE)
+
+ggplot(resOrdered, aes(x=log2FoldChange, y=neglogp)) +
+  geom_point(alpha=0.5) +
+  labs(
+    x = 'log2 fold change',
+    y = '-log10 p-value'
+  ) +
+  ylim(0, 5) +
+  theme_minimal()
+
+
+
+# this doesn't work
+
+files = c(
+  'selection_12_.txt', # protein
+  'selection_8_.txt'   # control
+)
+
+cts = create_table(files)
+
+coldata = data.frame(
+  condition = c(rep('protein', 1), rep('control', 1)),
+  row.names = colnames(cts)
+)
+coldata$condition = factor(coldata$condition)
+
+dds = DESeqDataSetFromMatrix(cts, coldata, ~ condition)
+
+group_size = 3
+filter = rowSums(counts(dds) >= 10) >= group_size
+dds = dds[filter, ]
+dds$condition = relevel(dds$condition, ref = 'control')
+
+dds = DESeq(dds)
+
+res = results(dds, name='condition_protein_vs_control')
+# plotCounts(dds, gene=which.min(res$padj), intgroup="condition")
+resLFC = lfcShrink(dds, coef='condition_protein_vs_control', type='apeglm')
+
+resOrdered = resLFC[order(resLFC$padj), ]
+resOrdered$neglogp = -log10(resOrdered$padj)
+mask = resOrdered$padj < 0.05
+resSign = resOrdered[!is.na(mask) & mask, ]
+
+head(as.data.frame(resSign))
+# write.csv(resSign, file = "stats.csv", row.names = TRUE)
+
+ggplot(resOrdered, aes(x=log2FoldChange, y=neglogp)) +
+  geom_point(alpha=0.5) +
+  labs(
+    x = 'log2 fold change',
+    y = '-log10 p-value'
+  ) +
+  ylim(0, 5) +
+  theme_minimal()
+
