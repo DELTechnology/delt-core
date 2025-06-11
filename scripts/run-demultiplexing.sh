abfbdddb074f6488c5a455ba35b2d7ba2efd273a
Adriano Martinelli <adrianom@student.ethz.ch>
2025-06-11 14:40:20 +0200

    run demultiplexing on all libraries

---
+++ scripts/run-demultiplexing.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for EVAL_DIR in /work/FAC/FBM/DBC/mrapsoma/prometex/data/DECLT-DB/experiments/eval-*; do
+  if [ -f "$EVAL_DIR/cutadapt_output_files/reads_with_adapters.gz" ]; then
+    echo "Skipping $EVAL_DIR: reads_with_adapters.gz already exists."
+    continue
+  fi
+
+  echo "Processing $EVAL_DIR"
+  delt-cli demultiplex run "$EVAL_DIR"/config.yml > "$EVAL_DIR/output.log" 2>&1
+  delt-cli qc report "$EVAL_DIR" > "$EVAL_DIR/qc_report.txt"
+  cat "$EVAL_DIR/qc_report.txt"
+done
